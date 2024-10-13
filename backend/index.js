46a5a42ddc337153fbab9b394e61a6bfc738cba5
Kenneth <baukenn@mfcgd.com>
2024-10-13 22:29:17 +0800

    -

--- backend/index.js
+++ backend/index.js
@@ -33,13 +33,13 @@ app.get("/", (req, res) => {
 
 app.use("/file-uploads", express.static("./file-uploads"));
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build")));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "client/build")));
 
-  app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "client/build", "index.html"));
-  });
-}
+//   app.get("*", function(req, res) {
+//     res.sendFile(path.join(__dirname, "client/build", "index.html"));
+//   });
+// }
 
 app.listen(port, () => {
   console.log(`Server is running at port: ${port}`);
