badbf1e2eb74899d28bb8de581951702dac01726
Kenneth <baukenn@mfcgd.com>
2024-10-20 21:07:13 +0800

    setup domain

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -3,8 +3,10 @@ module.exports = function(app) {
   app.use(
     ["/api"],
     createProxyMiddleware({
-      target: "http://localhost:5000"
-      // 'http://localhost:5000' || 'https://e-param-4179c775a38d.herokuapp.com:5000' || 'https://www.eparam.online',
+      target:
+        "http://localhost:5000" ||
+        "https://attendance-monitoring-aea3962f035e.herokuapp.com:5000" ||
+        "https://www.traceguard.online"
     })
   );
 };
